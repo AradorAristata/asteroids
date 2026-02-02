5c54ccbd3ba28cd177210e574862051cc81ea9cd
AradorAristata <aradoraristata@gmail.com>
2026-02-02 21:00:22 +0200

    C: asteroid fields

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -14,7 +16,13 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroidfield = AsteroidField()
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
@@ -26,7 +34,7 @@ def main():
         screen.fill("black")
 
         updatable.update(dt)
-        
+
         for item in drawable:
             item.draw(screen)
         
