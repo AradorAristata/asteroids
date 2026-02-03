476ec00c88efe5b677e324d2e62d6b943b93ba33
AradorAristata <aradoraristata@gmail.com>
2026-02-03 19:43:20 +0200

    D: Added collision and game over

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
+import sys
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -34,6 +35,12 @@ def main():
         screen.fill("black")
 
         updatable.update(dt)
+
+        for each in asteroids:
+            if player.collides_with(each):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
         for item in drawable:
             item.draw(screen)
